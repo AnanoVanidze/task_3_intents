dda76732754c2c487e8e77b513a11b2b66059389
Anano <vanidzeanano@gmail.com>
2022-09-22 15:53:32 +0400

    new way

--- app/src/main/java/com/example/task_3/MainActivity.kt
+++ app/src/main/java/com/example/task_3/MainActivity.kt
@@ -7,61 +7,76 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import com.example.task_3.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     private var imageUri: Uri? = null
 
-    private lateinit var binding: ActivityMainBinding
+    private val takePictureResult = registerForActivityResult(ActivityResultContracts.TakePicture()){
+            if(it){
+                imageUri?.let { uri ->
+                    binding.imageview.setImageURI(uri)
+                }
+            }
+        }
+
+
+    private val choosePictureResult = registerForActivityResult(ActivityResultContracts.GetContent()){
+        it?.let {
+            binding.imageview.setImageURI(it)
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+
         binding.takePictureBtn.setOnClickListener {
             takePicture()
-
         }
-
         binding.choosePictureBtn.setOnClickListener {
-            galleryChooseImage()
+           galleryChooseImage()
         }
+
     }
 
 
     private fun takePicture() {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivityForResult(intent,1)
 
+        getTmpFileUri().let { uri ->
+            imageUri = uri
+            takePictureResult.launch(uri)
+
+        }
     }
 
 
     private fun galleryChooseImage(){
-        val chooseIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
-        startActivityForResult(chooseIntent, 2)
-
+        choosePictureResult.launch("image/*")
     }
 
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
-        super.onActivityResult(requestCode, resultCode ,data)
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            if(data != null){
-                val imageBitmap: Bitmap? = data.getParcelableExtra("data")
-                binding.imageview.setImageBitmap(imageBitmap)
-            }
 
-        }else if(requestCode == 2 && resultCode == RESULT_OK){
-            if(data != null){
-                imageUri = data.data
-                binding.imageview.setImageURI(imageUri)
-            }
-
+    private fun getTmpFileUri(): Uri {
+        val tmpFile = File.createTempFile("tmp_image_file", ".png", cacheDir).apply {
+            createNewFile()
+            deleteOnExit()
         }
 
+        return FileProvider.getUriForFile(applicationContext, "${BuildConfig.APPLICATION_ID}.provider", tmpFile)
     }
+
+
+
 
 
 }
